92d4dc08d343938867023605009cac2868869d28
Iván <iquinterom1600@alumno.ipn.mx>
2021-10-11 07:35:31 -0500

    Final Version

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import { SearchIcon } from "@heroicons/react/outline";
+import Swal from "sweetalert2";
+
+const Toast = Swal.mixin({
+  icon: "error",
+  toast: true,
+  position: "top",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+});
 
 function App() {
+  const [song, setSong] = useState("");
+  const [artist, setArtist] = useState("");
+  const [title, setTitle] = useState("");
+  const [lyrics, setLyrics] = useState("");
+  const searchLyrics = () => {
+    if (!song || !artist) {
+      Toast.fire({ title: "Llene todos los campos" });
+    } else {
+      fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if(data.lyrics){
+            setTitle(song);
+            setLyrics(data.lyrics);
+          }
+          else{
+            Toast.fire({ title: "No se pudo encontrar esa letra" });
+          }
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    }
+  };
   return (
     <div className="min-h-screen bg-gray-200">
       <div className="text-center py-6 bg-white">
@@ -13,22 +47,35 @@ function App() {
             <input
               placeholder="Canción"
               className=" bg-gray-50 py-1 px-3 w-full rounded focus:outline-none focus:ring"
+              value={song}
+              onChange={(e) => setSong(e.target.value)}
             />
             <input
               placeholder="Artista"
               className=" bg-gray-50 py-1 px-3 w-full mt-4 rounded focus:outline-none focus:ring"
+              value={artist}
+              onChange={(e) => setArtist(e.target.value)}
             />
           </div>
           <div className="flex-none ml-4">
-            <button className="bg-blue-500 h-full px-4 rounded-md">
+            <button className="bg-blue-500 h-full px-4 rounded-md" onClick={searchLyrics}>
               <SearchIcon className="h-6 w-6 text-white" />
             </button>
           </div>
         </div>
+        {lyrics === "" ? (
+          <Fragment />
+        ) : (
           <div className="bg-white rounded-md mt-4 px-3 py-4 text-center">
-            <p className="font-medium text-2xl mb-2 pb-2 border-b-2">Titulo</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <p className="font-medium text-2xl mb-2 pb-2 border-b-2">{title}</p>
+            {lyrics
+              .split("\n")
+              .slice(1)
+              .map((paragraph, index) => (
+                <p key={index}>{paragraph}</p>
+              ))}
           </div>
+        )}
       </div>
     </div>
   );
